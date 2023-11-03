e43140932664781429bf0bb80184b24900456c7c
RadhikaPY <145678065+RadhikaPY@users.noreply.github.com>
2023-11-03 12:34:18 +0530

    Created recursive implementation logic for BowlingGame.java

--- BowlingGame.java
+++ BowlingGame.java
@@ -1,14 +1,61 @@
-    static Boolean check_if_sum_possible(ArrayList<Long> arr, Long k) {
-Collections.sort(arr);
-int n=arr.size();
-        return helper(arr, n, k);
+public class BowlingGame {
+    private int[] rolls;
+    private int currentRoll;
+
+    public BowlingGame() {
+        rolls=new int[21]; // maximum rolls in a game
+        currentRoll=0;
     }
-static Boolean helper(ArrayList<Long> arr, int index, Long k){
-     if(n>0 || k==0){
-        return false;
-    } 
-    if(k==0) return true;
-    return helper(arr, n-1, k) //exclude 
-    || helper(arr, n-1, k-arr.get(n-1));
-   
+
+    public void throwBall(int pins) {
+        rolls[currentRoll++]=pins;
     }
+
+    public int getScore() {
+        return calculateScore(0,0);
+    }
+
+    private int calculateScore(int frameIndex, int currentScore) {
+        if(frameIndex==10)
+            return currentScore;
+        int score=currentScore;
+        if(isStrike(frameIndex)) {
+            score+=10+strikeBonus(frameIndex);
+            return calculateScore(frameIndex+2, score);
+        } else if(isSpare(frameIndex)) {
+            score+=10+spareBonus(frameIndex);
+            return calculateScore(frameIndex+2, score);
+        } else {
+            score+=sumOfBallsInFrame(frameIndex);
+            return calculateScore(frameIndex+2, score);
+        }
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex]==10;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex]+rolls[frameIndex+1]==10;
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex]+rolls[frameIndex+1];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex+2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex+1]+rolls[frameIndex+2];
+    }
+
+    public static void main(String[] args) {
+        BowlingGame game=new BowlingGame();
+        game.throwBall(1);
+        game.throwBall(4);
+        int finalScore=game.getScore();
+        System.out.println("Final Score: "+finalScore);
+    }
+}
